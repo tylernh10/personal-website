c83b17d44f68443586dc33042880d7cdc543039f
Tyler Hinrichs <86966799+tylernh10@users.noreply.github.com>
2023-01-17 15:18:44 -0500

    updated nav functionality on small screens

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,19 @@
 function makeResponsive() {
-    var linkHolder = document.getElementById("nav-link-holder")
+    var linkHolder = document.getElementById("nav-link-holder");
     if (linkHolder.className === "link-holder") {
         linkHolder.className += " responsive";
     } else {
         linkHolder.className = "link-holder";
+    }
+}
+
+window.onclick = function(event) {
+    var linkHolder = document.getElementById("nav-link-holder");
+    if (linkHolder.classList.contains('responsive')) {
+        var parent = event.target.parentElement;
+        if (! (parent.classList.contains('header') || parent.parentElement.classList.contains('header'))) {
+            console.log((parent.classList.contains('header') || parent.parentElement.classList.contains('header')));
+            linkHolder.className = "link-holder";
+        }
     }
 }
