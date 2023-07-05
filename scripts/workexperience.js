7b09dbba32a7990d650b1be18a80383ee6ab27d4
Tyler Hinrichs <tylernh99@gmail.com>
2023-07-04 22:50:25 -0400

    v1.0.2

--- scripts/workexperience.js
+++ scripts/workexperience.js
@@ -2,23 +2,23 @@ var experienceInfo = [
     {
         image: "images/travelers-logo.png",
         title: "TLDP Intern • Travelers",
-        date: "Starting June 2023",
-        summary: "Incoming intern",
-        skills: ["Software Development"]
+        date: "June 2023 - present",
+        summary: "Reimagining audit system for Engineering Excellence team by creating a robust automation application leveraging Python, Selenium, and React JS; creating modular and reusable code through page object model design pattern and OOP principles; working in an Agile environment to methodically develop application",
+        skills: ["Python", "Selenium", "Automation", "Software Development", "Command Line Interface", "Graphical User Interface", "Agile", "Git/GitHub"]
     },
     {
         image: "images/wellinks-logo.png",
         title: "Junior Software Engineer • Wellinks",
         date: "August 2022 - March 2023",
         summary: "Android Developer, working collaboratively on a small team of engineers providing a digital solution for people with COPD, taking bug fixes, tech debt and new feature work on the Android app in an Agile environment, implementing third-party tools into the Android app such as Rollbar and Mixpanel, continued work on weather proof of concept, collaborating with multiple teams and sectors of the company such as Product, Clinical, and more.",
-        skills: ["Android Development", "Kotlin", "MVVM", "Git/GitHub", "Agile"]
+        skills: ["Android Development", "Kotlin", "MVVM", "Git/GitHub", "Agile", "Third-party software integration", "Cross-team collaboration", "Android SDK"]
     },
     {
         image: "images/wellinks-logo.png",
         title: "Software Engineering Intern • Wellinks",
         date: "June 2022 - August 2022",
         summary: "Contributed as an Android developer, working on bugs, technical debt, and new feature work, maintained high-quality code and architecture using Kotlin alongside Hilt, MVVM Design Pattern, Retrofit, GraphQL Querying, fullstack Android development, OOP, REST APIs, working in an agile Environment, worked on \"Today's Weather\" proof of concept weather feature in the Wellinks Android app giving comprehensive environmental insights based on local weather metrics tailored to people with COPD.",
-        skills: ["Android Development", "Kotlin", "MVVM", "Git/GitHub", "Agile"]
+        skills: ["Android Development", "Kotlin", "MVVM", "Git/GitHub", "Agile", "OOP", "REST APIs", "Fullstack Software Development", "Android SDK"]
     },
 ]
 
