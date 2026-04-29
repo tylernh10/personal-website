00f65f4cce3fd866cd3a7e4fe37883e064a40210
Tyler Hinrichs <tylernh99@gmail.com>
2026-04-29 18:12:44 -0400

    add GitHub releases link to footer

--- components/Common/Footer/Footer.tsx
+++ components/Common/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 export default function Footer() {
     return (
         <div className="w-fit mx-auto flex flex-col justify-center items-center p-4 mb-2 text-muted-foreground">
-            <div>v2.0.0</div>
+            <Link href="https://github.com/tylernh10/personal-website/releases" target="_blank">v2.0.0</Link>
             <div>© {new Date().getFullYear()} Tyler Hinrichs</div>
         </div>
     )
